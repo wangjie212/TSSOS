2f68aa9f4da599d1c5f584d49913b24fe5a9ae49
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2019-11-06 23:43:02 +0800

    Add files via upload

--- src/BlockPOP.jl
+++ src/BlockPOP.jl
@@ -9,4 +9,6 @@ using LightGraphs
 include("blockpop_cons.jl")
 include("blockpop_uncons.jl")
 
+export newton_basis, get_basis, get_blocks, blockupop, get_cliques, get_hblocks!, get_hcliques!, blockupop_first, blockupop_higher!, blockcpop_first, blockcpop_higher!
+
 end
