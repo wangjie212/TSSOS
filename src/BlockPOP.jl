c5281bd46e687b0c2968c180e24e093f6428a3d6
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2019-11-06 23:52:20 +0800

    Add files via upload

--- src/BlockPOP.jl
+++ src/BlockPOP.jl
@@ -6,9 +6,9 @@ using MosekTools
 using JuMP
 using LightGraphs
 
+export newton_basis, get_basis, get_blocks, blockupop, get_cliques, get_hblocks!, get_hcliques!, blockupop_first, blockupop_higher!, blockcpop_first, blockcpop_higher!
+
 include("blockpop_cons.jl")
 include("blockpop_uncons.jl")
-
-export newton_basis, get_basis, get_blocks, blockupop, get_cliques, get_hblocks!, get_hcliques!, blockupop_first, blockupop_higher!, blockcpop_first, blockcpop_higher!
 
 end
