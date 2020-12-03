07752122cd7e146a61a5d27aea87187b851c8a14
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2020-12-03 23:33:28 +0800

    CS="MD" -> CS="MF"

--- src/blockmix.jl
+++ src/blockmix.jl
@@ -24,7 +24,7 @@ mutable struct mdata_type
     sizes # the number of different blocks
 end
 
-function cs_tssos_first(pop,x,d;nb=0,numeq=0,CS="MD",minimize=false,assign="min",TS="block",QUIET=false,solve=true,solution=false,MomentOne=true)
+function cs_tssos_first(pop,x,d;nb=0,numeq=0,CS="MF",minimize=true,assign="min",TS="block",QUIET=false,solve=true,solution=false,MomentOne=true)
     n=length(x)
     m=length(pop)-1
     coe=Array{Vector{Float64}}(undef, m+1)
@@ -57,7 +57,7 @@ function cs_tssos_first(pop,x,d;nb=0,numeq=0,CS="MD",minimize=false,assign="min"
     return opt,sol,data
 end
 
-function cs_tssos_first(supp::Vector{SparseMatrixCSC{UInt8,UInt32}},coe::Vector{Vector{Float64}},n::Int,d,dg::Vector{Int};nb=0,numeq=0,CS="MD",minimize=false,assign="min",TS="block",QUIET=false,solve=true,solution=false,MomentOne=true)
+function cs_tssos_first(supp::Vector{SparseMatrixCSC{UInt8,UInt32}},coe::Vector{Vector{Float64}},n::Int,d,dg::Vector{Int};nb=0,numeq=0,CS="MF",minimize=true,assign="min",TS="block",QUIET=false,solve=true,solution=false,MomentOne=true)
     m=length(supp)-1
     cliques,cql,cliquesize=clique_decomp(n,m,dg,supp,order=d,alg=CS,minimize=minimize)
     I,ncc=assign_constraint(m,supp,cliques,cql,cliquesize,assign=assign)
