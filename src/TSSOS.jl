ab507f03662e5d58d1e5680624f3eadff1404ba2
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2021-02-21 02:49:39 +0800

    Add files via upload

--- src/TSSOS.jl
+++ src/TSSOS.jl
@@ -11,7 +11,6 @@ using MetaGraphs
 using SemialgebraicSets
 using COSMO
 using SDPT3
-using NCTSSOS
 
 export tssos_first, tssos_higher!, cs_tssos_first, cs_tssos_higher!, local_solution, refine_sol,
 nctssos_first, nctssos_higher!, cs_nctssos_first, cs_nctssos_higher!
@@ -23,5 +22,7 @@ include("blockpop_cons.jl")
 include("nblockmix.jl")
 include("complex.jl")
 include("local_solution.jl")
+include("NCTSSOS/NCTSSOS.jl")
+using .NCTSSOS
 
 end
