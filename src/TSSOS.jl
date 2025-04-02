59fae94dfc7215e8b6c7cd513242adb2ab3ce039
Jie Wang <wangjie212@amss.ac.cn>
2025-04-02 22:16:32 +0800

    fix bugs

--- src/TSSOS.jl
+++ src/TSSOS.jl
@@ -1,6 +1,5 @@
 module TSSOS
 
-# using ChordalGraph
 using MosekTools
 using JuMP
 using Graphs
@@ -15,12 +14,11 @@ using Dualization
 using Printf
 using AbstractAlgebra
 using Random
-using ChordalGraph
 # using Hypatia
 # using SDPT3
 # using SDPNAL
 
-# import CliqueTrees
+import CliqueTrees
 
 export tssos_first, tssos_higher!, cs_tssos_first, cs_tssos_higher!, local_solution, refine_sol,
 cosmo_para, mosek_para, add_psatz!, add_poly!, get_nbasis, get_moment, get_moment_matrix, get_cmoment, homogenize, 
@@ -29,49 +27,49 @@ show_blocks, complex_to_real, add_SOSMatrix!, sparseobj, get_mmoment, extract_so
 extract_solutions_pmo, extract_solutions_robust_pmo, extract_weight_matrix, add_SOS!, tssos_symmetry,
 run_H1, run_H1CS, run_H2, run_H2CS, construct_CDK, construct_marginal_CDK, construct_CDK_cs, construct_marginal_CDK_cs
 
-# function chordal_cliques!(G; method="MF", minimize=false)
-#     # choose algorithm
-#     alg = method == "MF" && minimize ? CliqueTrees.MinimalChordal(CliqueTrees.MF())  :
-#           method == "MD" && minimize ? CliqueTrees.MinimalChordal(CliqueTrees.MMD()) :
-#           method == "MF" && !minimize ? CliqueTrees.MF()                              :
-#           method == "MD" && !minimize ? CliqueTrees.MMD()                             :
-#           error()
+function chordal_cliques!(G; method="MF", minimize=false)
+    # choose algorithm
+    alg = method == "MF" && minimize ? CliqueTrees.MinimalChordal(CliqueTrees.MF())  :
+          method == "MD" && minimize ? CliqueTrees.MinimalChordal(CliqueTrees.MMD()) :
+          method == "MF" && !minimize ? CliqueTrees.MF()                              :
+          method == "MD" && !minimize ? CliqueTrees.MMD()                             :
+          error()
 
-#     # compute maximal cliques
-#     label, tree = CliqueTrees.cliquetree(G; alg)
+    # compute maximal cliques
+    label, tree = CliqueTrees.cliquetree(G; alg)
     
-#     # triangulate graph
-#     F = CliqueTrees.FilledGraph(tree)
+    # triangulate graph
+    F = CliqueTrees.FilledGraph(tree)
     
-#     for edge in edges(F)
-#         add_edge!(G, label[src(edge)], label[dst(edge)])
-#     end
+    for edge in edges(F)
+        add_edge!(G, label[src(edge)], label[dst(edge)])
+    end
     
-#     # return maximal cliques
-#     maximal_cliques = Vector{Vector{UInt16}}(undef, length(tree))
+    # return maximal cliques
+    maximal_cliques = Vector{Vector{UInt16}}(undef, length(tree))
     
-#     for (i, clique) in enumerate(tree)
-#         maximal_cliques[i] = sort!(label[clique])
-#     end
+    for (i, clique) in enumerate(tree)
+        maximal_cliques[i] = sort!(label[clique])
+    end
     
-#     cliquesize = length.(maximal_cliques)
-#     cql = length(cliquesize)
-#     return maximal_cliques, cql, cliquesize
-# end
+    cliquesize = length.(maximal_cliques)
+    cql = length(cliquesize)
+    return maximal_cliques, cql, cliquesize
+end
 
-# function add_clique!(G, nodes)
-#     for i in 1:length(nodes)-1, j in i+1:length(nodes)
-#         add_edge!(G, nodes[i], nodes[j])
-#     end
-# end
+function add_clique!(G, nodes)
+    for i in 1:length(nodes)-1, j in i+1:length(nodes)
+        add_edge!(G, nodes[i], nodes[j])
+    end
+end
 
-# function max_cliques(G)
-#     cliques = convert(Vector{Vector{UInt16}}, maximal_cliques(G))
-#     sort!.(cliques)
-#     cliquesize = length.(cliques)
-#     cql = length(cliquesize)
-#     return cliques,cql,cliquesize
-# end
+function max_cliques(G)
+    cliques = convert(Vector{Vector{UInt16}}, maximal_cliques(G))
+    sort!.(cliques)
+    cliquesize = length.(cliques)
+    cql = length(cliquesize)
+    return cliques,cql,cliquesize
+end
 
 include("clique_merge.jl")
 include("blockpop_uncons.jl")
@@ -87,8 +85,8 @@ include("matrixsos.jl")
 include("sum_of_ratios.jl")
 include("utils.jl")
 include("CDK.jl")
-# include("symmetry.jl")
-# include("SymbolicWedderburn/SymbolicWedderburn.jl")
-# using .SymbolicWedderburn
+include("symmetry.jl")
+include("SymbolicWedderburn/SymbolicWedderburn.jl")
+using .SymbolicWedderburn
 
 end
